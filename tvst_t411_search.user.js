090fbca7a060195d7575f9892520817e9df33a45
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2015-09-24 01:39:52 +0200

    changed domain name from t411.io to t411.in

--- tvst_t411_search.user.js
+++ tvst_t411_search.user.js
@@ -8,10 +8,16 @@
 // @require     https://code.jquery.com/jquery-2.1.3.min.js
 // ==/UserScript==
 
-this.jQuery = jQuery.noConflict(true);
-var search_base_link = 'http://www.t411.io/torrents/search/?search=';
+// Changelog:
+// 24/09/2015	changed domain name from t411.io to t411.in
+
+var t411_domain = 'www.t411.in'
+
+var search_base_link = 'http://' + t411_domain + '/torrents/search/?search=';
 var search_prefix = '';
 var search_suffix = '';
+
+this.jQuery = jQuery.noConflict(true);
 
 jQuery(document).ready(function() {
   jQuery("<style type='text/css'> .download-link{display:inline;width:8px;height:8px;margin:0 auto;background:url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAAAOCAQAAAC1QeVaAAAAyElEQVQY02NggAA2+fi0/xCoEsmADuR8yJRklAuESSrHMjBCRVm47OcC4TyvfTBJn8N280Fi7EIMTJwaM2HCyFB7PosA0EBmGeWJ6FLqM1iUoIYzicm2JH6HSSR+V+xmkkJ2j6BkSfgzkFT4C5lqRlGYOCsDC5gWkswPvBf6RKYMKsUClNGr1shjEIRIS0TIJjJApATUMvWqGYB2fDWewiCK4msRg974j2n/GRQKgdJfPA977USCh0BSKpUMjDzSadj8KZ/PJAAAUPCAT2cQ2usAAAAASUVORK5CYII=') no-repeat 0 0} </style>").appendTo("head");
